2a5f6f1ef1c153a962f62f8e1882d4dd92fc6af4
bogarimarcel <marcel.branea@outlook.com>
2022-11-02 21:58:38 +0200

    char to int number limitation

--- CharToInt/CharToInt.cpp
+++ CharToInt/CharToInt.cpp
@@ -2,31 +2,49 @@
 
 using namespace std;
 
-int main()
+int CharToInt(char c[])
 {
-	char c[] = "123";
 	int decNr = 0;
 	int temp = 1;
 	int lenStr = 0;
 	int i = 0;
-
+	int check = 0;
 	// calculam lungimea sirului 
 	while (c[i] != NULL)
 	{
+		if (c[i] < '0' || c[i] > '9')
+		{
+			check = 1;
+		}
+		
 		i++;
+
 	}
-
-	lenStr = i;
-
-	// valoarea ASCII a lui a este 'a'
-	// valoare ASCII a unui caracter escape, de ex \, este '\\' , pt TAB este '\TAB'
-
-	for (i = lenStr - 1; i >= 0; i--)
+	if (check == 1)
 	{
-		decNr = decNr + (c[i] - '0') * temp;
-
-		temp = temp * 10;
+		cout << "only numbers are allowed";
 	}
+	else
+	{
+		lenStr = i;
 
-	cout << decNr;
+		// valoarea ASCII a lui a este 'a'
+		// valoare ASCII a unui caracter escape, de ex \, este '\\' , pt TAB este '\TAB'
+
+		for (i = lenStr - 1; i >= 0; i--)
+		{
+			decNr = decNr + (c[i] - '0') * temp;
+
+			temp = temp * 10;
+		}
+		cout << decNr;
+	}
+	
+	return 0;
+}
+
+int main()
+{
+	char c[] = "1234gv56";
+	CharToInt(c);
 }
